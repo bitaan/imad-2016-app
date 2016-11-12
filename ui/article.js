4f1bfc4faf96d94664e7131535959d5e9c13298c
bitaan <bitaan96@gmail.com>
2016-11-12 21:42:56 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -10,7 +10,7 @@ function loadArticles () {
                 for (var i=0; i< articleData.length; i++) {
                     content += `<li>
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
-                    (${articleData[i].date.split('T')[0]})</li><br>`;
+                    (${articleData[i].date.split('T')[0]})</li><br><br>`;
                 }
                 content += "</ul>";
                 articles.innerHTML = content;
