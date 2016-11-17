db90b8ec4f7ebe21f3834f37ebd183752136aced
BITAAN CHAKRABARTI <bitaan96@gmail.com>
2016-11-17 20:03:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ var bodyParser = require('body-parser');
 var session = require('express-session');
 
 var config = {
-    user: 'postgres',
-    database: 'postgres',
-    host: 'localhost',
+    user: 'bitaan',
+    database: 'bitaan',
+    host: 'db.imad.hasura-app.io',
     port: '5432',
-    password: 'bitaan18'
+    password: process.env.DB_PASSWORD
 };
 
 var app = express();
